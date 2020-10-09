9d49016b8ef4bdde3316ed15f3e76571e7524582
zyme <lolrichyisswag@gmail.com>
2020-10-09 11:24:30 -0500

    added api keys, and now it can tweet

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const Twit = require('twit')
-const dotenv = require('dotenv')
+require('dotenv').config()
 
-var T = new Twit({
-  consumer_key:         process.env.CONSUMER_KEY,
-  consumer_secret:      process.env.CONSUMER_SECRET,
-  access_token:         process.env.ACCESS_TOKEN,
-  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
-  strictSSL:            true,     // i want to do some more verification stuff i guess
+var T = new Twit({ // grab api keys from a .env 
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  bearer_token: process.env.BEARER_TOKEN
 })
 
+
+T.post('statuses/update', { status: 'hello world! this is shibe bots first "tweet"' }, function(err, data, response) {
+  console.log(err)
+})
