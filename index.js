06f63be9fffb605879dd952b484a82d14493e21b
zyme <lolrichyisswag@gmail.com>
2020-10-09 11:31:12 -0500

    why did i commit this

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Twit = require('twit')
 require('dotenv').config()
 const axios = require("axios");
+// code above gets modules
 
-var T = new Twit({ // grab api keys from a .env 
+var T = new Twit({ // grab api keys from a .env file for security
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
@@ -10,7 +11,7 @@ var T = new Twit({ // grab api keys from a .env
   bearer_token: process.env.BEARER_TOKEN
 })
 
-
+// code below just makes a tweet
 T.post('statuses/update', {
   status: 'hello world! this is shibe bots first "tweet"'
 }, function (err, data, response) {
