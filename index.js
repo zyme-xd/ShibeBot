a8a55cc391884e9e6a3e057993845f8765d827f1
zyme <lolrichyisswag@gmail.com>
2020-10-08 23:54:37 -0500

    looks normal now lol

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Twit = require('twit')
 const dotenv = require('dotenv')
 
 var T = new Twit({
-  consumer_key:         ,
-  consumer_secret:      ,
-  access_token:         ,
-  access_token_secret:  ,
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
   strictSSL:            true,     // i want to do some more verification stuff i guess
 })
 
