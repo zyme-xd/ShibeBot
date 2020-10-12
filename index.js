83f6c3f855bcd5e8ae901cbe083a56245dce47bb
zyme <lolrichyisswag@gmail.com>
2020-10-12 15:19:53 -0500

    basic version, not complete

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Twit = require('twit')
 require('dotenv').config()
 const axios = require("axios");
+const {
+  DownloaderHelper
+} = require('node-downloader-helper');
+const fs = require("fs")
 // code above gets modules
-
+const folder = "./folder"
 var T = new Twit({ // grab api keys from a .env file for security
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -11,9 +15,58 @@ var T = new Twit({ // grab api keys from a .env file for security
   bearer_token: process.env.BEARER_TOKEN
 })
 
-// code below just makes a tweet
-T.post('statuses/update', {
-  status: 'hello world! this is shibe bots first "tweet"'
-}, function (err, data, response) {
-  console.log(data)
-})
+function getNewDog() {
+  axios.get("https://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true").then(resp => {
+    let imageHourly = resp.data[0]
+    console.log(imageHourly + " Is the image of the hour. Beginning tweet process.")
+    var dl = new DownloaderHelper(imageHourly, './folder', );
+    dl.on('end', () => console.log('Download Completed'))
+    dl.start();
+  })
+}
+
+
+function SeeContents() {
+  getNewDog()
+  fs.readdirSync(folder).forEach(file => {
+    console.log(file);
+    var filepath = "./folder/" + file
+    dogpost()
+
+    function dogpost() {
+      global.b64content = fs.readFileSync(filepath, {
+        encoding: 'base64'
+      })
+      T.post('media/upload', {
+        media_data: b64content
+      }, function (err, data, response) {
+
+        var mediaIdStr = data.media_id_string
+        var altText = "bark."
+        var meta_params = {
+          media_id: mediaIdStr,
+          alt_text: {
+            text: altText
+          }
+        }
+
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+          if (!err) {
+            var params = {
+              status: 'Hourly Dog',
+              media_ids: [mediaIdStr]
+            }
+
+            T.post('statuses/update', params, function (err, data, response) {
+              console.log(data)
+            })
+          }
+        });
+      })
+    }
+  })
+}
+
+
+
+setInterval(SeeContents, 1000)
